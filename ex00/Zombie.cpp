d6b1e6c27632e1ff689b807da93b1f26f21e1675
Berke Ibrahim Ince <bince@made-f0Ar1s4.clusters.42paris.fr>
2024-11-12 19:43:52 +0100

    ex00 is being done

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,15 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+Zombie::~Zombie()
+{
+	std::cout << this->name <<" died. (again)." << std::endl;
+}
+void Zombie::announce()
+{
+	std::cout<< this->name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
---
+++ ex00/main.cpp
@@ -0,0 +1,8 @@
+#include "Zombie.hpp"
+
+int main()
+{
+	Zombie zombie("steve");
+	Zombie *z1 = newZombie("z12");
+	zombie.announce();
+}
